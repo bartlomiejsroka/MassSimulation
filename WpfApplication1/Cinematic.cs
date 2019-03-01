f0fcf596fcf42e0a2cead2c6db85c382de055a7d
bartotex <bartlomiej.sroka19@gmail.com>
2019-03-01 01:45:11 +0100

    dzialajaca metoda hamowania

--- WpfApplication1/Cinematic.cs
+++ WpfApplication1/Cinematic.cs
@@ -9,7 +9,7 @@ namespace WpfApplication1
     class Cinematic
     {
         private double mass, friction;
-        private double velocity;
+        private double finalVelocity;
         public Cinematic(double mass, double friction)
         {
             this.mass = mass;
@@ -18,10 +18,15 @@ namespace WpfApplication1
         public double CalculatePos(double time)
         {
             var pos =(time / friction )+ (mass/(friction*2))*(Math.Exp(-( friction * time) / mass)) - mass/(2*friction);
+            finalVelocity = Velocity(time);
             return pos;
-           
         }
         public double NoforcePosision(double time)
+        {
+            var vel = finalVelocity *( (1 / friction) - (1 / friction) * Math.Exp((-friction * time) / mass));
+            return vel;
+        }
+        private double Velocity(double time)
         {
             var vel = (1 / friction) - (1 / friction) * Math.Exp((-friction * time) / mass);
             return vel;
